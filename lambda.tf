775b7693d362dfc119082b33a6f69f24741b3c46
Aditya Murali <adityamurali2@gmail.com>
2025-06-17 20:59:35 +1000

    Moved lambda zip

--- lambda.tf
+++ lambda.tf
@@ -47,7 +47,7 @@ resource "aws_lambda_function" "security_checker" {
   handler          = "lambda_function.lambda_handler"
   role             = aws_iam_role.lambda_exec_role.arn
   filename         = "lambda_function.zip"
-  source_code_hash = filebase64sha256("lambda_function.zip")
+  source_code_hash = filebase64sha256("lambda/lambda_function.zip")
 
   environment {
     variables = {

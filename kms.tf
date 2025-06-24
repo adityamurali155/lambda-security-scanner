5cf47144bd65062f3214a3ac7603d79faa94931b
Aditya Murali <adityamurali2@gmail.com>
2025-06-24 18:59:22 +1000

    Fixed KMS errors

--- kms.tf
+++ kms.tf
@@ -2,38 +2,6 @@ resource "aws_kms_key" "dynamodb_key" {
   description             = "KMS key for encrypting DynamoDB table"
   deletion_window_in_days = 10
   enable_key_rotation     = true
-
-    policy = jsonencode({
-    Version = "2012-10-17",
-    Id      = "lambda-only-policy",
-    Statement: [
-      # Allow Lambda's IAM role full KMS access
-      {
-        Sid: "AllowLambdaUse",
-        Effect: "Allow",
-        Principal: {
-          AWS: aws_iam_role.lambda_exec.arn
-        },
-        Action: [
-          "kms:Encrypt",
-          "kms:Decrypt",
-          "kms:ReEncrypt*",
-          "kms:GenerateDataKey*"
-        ],
-        Resource: "*"
-      },
-      # Allow the root user in the account to administer the key
-      {
-        Sid: "AllowRootAccountAdmin",
-        Effect: "Allow",
-        Principal: {
-          AWS: "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        },
-        Action: "kms:*",
-        Resource: "*"
-      }
-    ]
-  })
 }
 resource "aws_kms_alias" "dynamodb_key_alias" {
   name          = "alias/dynamodbKey"
